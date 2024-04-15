33d6259cde09b62f2c17863c908f75388a58df30
Jose Quintero (WPOSS) <josequintero@wposs0.onmicrosoft.com>
2024-04-15 08:49:09 -0500

    Ajustes en la parte de la card de la comida

--- src/app/shared/layouts/card-food/card-food.component.ts
+++ src/app/shared/layouts/card-food/card-food.component.ts
@@ -10,7 +10,7 @@ export class CardFoodComponent {
   Food: any[] = [
     {
       food_id: 1,
-      food_restaurant: 'Ruca',
+      food_restaurant: 'Cine',
       food_name: 'Palomitas pequeñas',
       food_flavor: 'Mantequilla',
       food_discount: 0,
@@ -25,6 +25,33 @@ export class CardFoodComponent {
       food_price: 10000,
       food_discount: 0,
       food_img: 'assets/img/food/hamburguesa.jpg'
+    },
+    {
+      food_id: 3,
+      food_restaurant: 'La provincia',
+      food_name: 'Pastas carbonara',
+      food_flavor: 'carbonara',
+      food_discount: 0,
+      food_price: 12000,
+      food_img: 'assets/img/food/pastas.jpg'
+    },
+    {
+      food_id: 4,
+      food_restaurant: 'Pizza-Juan-XXIII',
+      food_name: 'Pizza personal',
+      food_flavor: 'americana',
+      food_price: 6000,
+      food_discount: 0,
+      food_img: 'assets/img/food/pizza.jpg'
+    },
+    {
+      food_id: 5,
+      food_restaurant: 'Pascual',
+      food_name: 'Pincho mixto',
+      food_flavor: 'normal',
+      food_price: 14000,
+      food_discount: 0,
+      food_img: 'assets/img/food/pinchos.jpg'
     },
   ]
 
