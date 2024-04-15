cf128fd099008998957516274da3e485642b51ce
Jose Quintero (WPOSS) <josequintero@wposs0.onmicrosoft.com>
2024-04-15 09:43:44 -0500

    Funcionalidad de colocar la pantalla en pantalla-completa

--- src/app/shared/layouts/nav/nav.component.ts
+++ src/app/shared/layouts/nav/nav.component.ts
@@ -7,7 +7,30 @@ import {Component} from '@angular/core';
 })
 export class NavComponent {
 
+  isFullScreen: boolean = false;
+
   constructor() {
+  }
+
+  toggleFullScreen(): void {
+    if (!this.isFullScreen) {
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if ((document.documentElement as any).webkitRequestFullscreen) {
+        (document.documentElement as any).webkitRequestFullscreen();
+      } else if ((document.documentElement as any).msRequestFullscreen) {
+        (document.documentElement as any).msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if ((document as any).webkitExitFullscreen) {
+        (document as any).webkitExitFullscreen();
+      } else if ((document as any).msExitFullscreen) {
+        (document as any).msExitFullscreen();
+      }
+    }
+    this.isFullScreen = !this.isFullScreen;
   }
 
 }
