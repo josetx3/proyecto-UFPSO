028d5596bce42e3dd5526e84f20a5efa69973712
Jose Quintero (WPOSS) <josequintero@wposs0.onmicrosoft.com>
2024-05-25 12:42:14 -0500

    Estilos principales del order-summary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {RegisterComponent} from "@app/modules/user/auth/pages/register/register.component";
+import {OrderSummaryComponent} from "@app/shared/layouts/order-summary/order-summary.component";
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'administration',
     loadChildren: () => import('@app/modules/administration/administration.module').then(m => m.AdministrationModule)
+  },
+  {
+    path: 'order-summary',
+    component: OrderSummaryComponent
   }
 ];
 
